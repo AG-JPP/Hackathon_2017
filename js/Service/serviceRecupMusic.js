d785e415069ff610a81adec16188497e78c91487
giovanne3u <giovannelli.alexis@gmail.com>
2017-03-10 22:04:06 +0100

    Ajout param ressource

--- js/Service/serviceRecupMusic.js
+++ js/Service/serviceRecupMusic.js
@@ -26,7 +26,7 @@ app.service('TrackPopularityService',['Tracks',function (Tracks) {
 app.service('TrackSearchService',['Tracks',function (Tracks) {
     this.tracksSearch = [];
     this.search= function () {
-        this.tracksSearch = Tracks.recherche(function (success) {
+        this.tracksSearch = Tracks.recherche({namesearch : $scope.search.namesearch, artist_name : $scope.search.artist_name, album_name : $scope.search.artist_name}, function (success) {
             success.forEach(function (element) {
                 this.tracksSearch.push(element);
             })
@@ -42,7 +42,7 @@ app.service('TrackSearchService',['Tracks',function (Tracks) {
 app.service('TrackSearchTypeService',['Tracks',function (Tracks) {
     this.tracksType = [];
     this.searchType = function () {
-        this.tracksType = Tracks.rechercheGenre(function (success) {
+        this.tracksType = Tracks.rechercheGenre({tags : scope.search.genre}, function (success) {
             success.forEach(function (element) {
                 this.tracksType.push(element);
             })
