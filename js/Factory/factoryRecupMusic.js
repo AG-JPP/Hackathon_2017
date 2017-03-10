caa3172db06c45339f9c564436cf603ab4d10afe
postelu <romain.postel@etu.univ-lorraine.fr>
2017-03-11 00:21:07 +0100

    discover and popular works

--- js/Factory/factoryRecupMusic.js
+++ js/Factory/factoryRecupMusic.js
@@ -8,7 +8,7 @@
 app.factory("Tracks", ['$resource', 'api', function ($resource, api) {
     return $resource(api.url+"&id=:id", {id: '@_id'},
         {
-            populaire: {method: 'GET', url: api.url + '&order=popularity_total&limit=8'},
+            populaire: {method: 'GET', url: api.url + '&order=popularity_total&limit=12'},
             recherche: {method: 'GET', url: api.url + '&namesearch=:name&artist_name=:name&album_name=:name'},
             rechercheGenre: {method: 'GET', url: api.url + '&tags=:genre'},
             buzzrate : {method : 'GET', url: api.url + '&boost=buzzrate&limit=6'}
