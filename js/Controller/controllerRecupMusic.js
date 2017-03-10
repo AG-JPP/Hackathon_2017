e9a2fff3bccb90582174da2d6560e699675aa1bc
giovanne3u <giovannelli.alexis@gmail.com>
2017-03-10 19:47:39 +0100

    Controllers

--- js/Controller/controllerRecupMusic.js
+++ js/Controller/controllerRecupMusic.js
@@ -3,11 +3,32 @@
  */
 
 //Controller
-app.controller("MainController",['$scope', 'Tracks', 'TrackPopularityService'function ($scope,Tracks, TrackPopularityService) {
+app.controller("PopularController",['$scope', 'Tracks', 'TrackPopularityService', function ($scope,Tracks, TrackPopularityService) {
 
     $scope.getPopulairesTracks = function(){
       var tracks = TrackPopularityService.getPopulairesTracks();
       console.log(tracks);
     }
+
+}]);
+
+app.controller("SearchController", ['$scope', 'Tracks', 'TrackSearchService', function($scope, Tracks, TrackSearchService){
+
+    $scope.searchTracks = function(){
+      TrackSearchService.search();
+      var track = TrackSearchService.getTracks();
+      console.log(track);
+    }
+}]);
+
+app.controller("SearchTypeController", ['$scope', 'Tracks', 'TrackSearchTypeService', function($scope, Tracks, TrackSearchTypeService){
+
+  $scope.searchTrackType = function(){
+    TrackSearchTypeService.searchType();
+    var tracks = TrackSearchTypeService.getTracks();
+    console.log(tracks);
+
+
+  }
 
 }]);
